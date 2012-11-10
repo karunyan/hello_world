35cedab4a65c2eff6bb6f2cadf997cddea8a6c2e
Karunyan Krishnasamy <karun.krishna@gmail.com>
2012-11-10 18:46:15 -0500

    Create ojectProgramming Project to practice OO programming

---
+++ objectProgramming/src/Dog.java
@@ -0,0 +1,44 @@
+public class Dog {
+int size; 
+String name;
+
+static String BIG_BARK = "Wooof! Wooof!";
+static String MEDIUM_BARK = "Ruff! Ruff!";
+static String SMALL_BARK = "Yip! Yip!";
+
+
+void bark()
+{
+	if (size > 60){
+		System.out.println(name + " barked, " + BIG_BARK); 
+		 
+	} 
+	else if (size > 14) {
+		System.out.println(name + " barked, " + MEDIUM_BARK);
+	}
+	else {
+		System.out.println(name + " barked, " + SMALL_BARK);
+	}		
+}
+
+
+public int getSize() {
+	return size;
+}
+
+
+public void setSize(int size) {
+	this.size = size;
+}
+
+
+public String getName() {
+	return name;
+}
+
+
+public void setName(String name) {
+	this.name = name;
+}
+
+}
