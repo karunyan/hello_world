ce19da35ce5dd0a7048402bb33f749071f367c40
Karunyan Krishnasamy <karun.krishna@gmail.com>
2012-11-11 19:23:17 -0500

    Add Test and business logic for SimpleDotComGame

--- SimpleDotComGame/src/SimpleDotCom.java
+++ SimpleDotComGame/src/SimpleDotCom.java
@@ -1,4 +1,35 @@
 
 public class SimpleDotCom {
 
+	private int numOfHits = 0;
+	private int[] locationCells;
+	
+public String checkYourself(String userGuess)
+{
+	int guess = Integer.parseInt(userGuess);
+	String result = "miss";
+	
+	for (int cell : locationCells)
+	{
+		if (guess == cell){
+			result = "hit";
+			numOfHits++;
+			break;
+		}
+	}
+	if (numOfHits == locationCells.length){
+		result="kill";
+	}
+	
+	System.out.println(result);
+	return result;
+	
+}
+
+public void setLocationCells(int[] locationCells) {
+	this.locationCells = locationCells;
+}
+
+
+
 }
