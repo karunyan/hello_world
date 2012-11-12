af9f98ea284f07c9b9d43730dfd6cc618f10da7c
Karunyan Krishnasamy <karun.krishna@gmail.com>
2012-11-11 23:16:03 -0500

    Updated SimpleDotCom

--- SimpleDotComGame/src/SimpleDotComeTestDrive.java
+++ SimpleDotComGame/src/SimpleDotComeTestDrive.java
@@ -1,11 +1,20 @@
+import java.io.*;
 
 public class SimpleDotComeTestDrive {
 
 	public static void main (String[] args){
 		
-		SimpleDotCom dot = new SimpleDotCom();
+		int numOfGuesses = 0;
+		int randomNum = (int)(Math.random()*5);
 		int[] locations = {2,3,4};
+
+		
+		SimpleDotCom dot = new SimpleDotCom();
 		dot.setLocationCells(locations);
+		
+		//InputStreamReader kGuess = new InputStreamReader(System.in);
+		//BufferedReader userGuess = new BufferedReader(kGuess);
+		
 		
 		String userGuess="2";
 		String result = dot.checkYourself(userGuess);
