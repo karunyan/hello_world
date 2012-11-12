6dc189dacc7b18fc9516430c8d052d4475bf16df
Karunyan Krishnasamy <karun.krishna@gmail.com>
2012-11-12 13:47:16 -0500

    Add userinput code

--- SimpleDotComGame/src/SimpleDotComeTestDrive.java
+++ SimpleDotComGame/src/SimpleDotComeTestDrive.java
@@ -1,30 +1,31 @@
-import java.io.*;
-
 public class SimpleDotComeTestDrive {
 
-	public static void main (String[] args){
-		
-		int numOfGuesses = 0;
-		int randomNum = (int)(Math.random()*5);
-		int[] locations = {2,3,4};
+	public static void main(String[] args) {
 
+		int numOfGuesses = 0;
+		int randomNum = (int) (Math.random() * 5);
+		int[] locations = { randomNum, randomNum + 1, randomNum + 2 };
+		boolean isAlive = true;
+		GameHelper helper = new GameHelper();
 		
 		SimpleDotCom dot = new SimpleDotCom();
 		dot.setLocationCells(locations);
-		
-		//InputStreamReader kGuess = new InputStreamReader(System.in);
-		//BufferedReader userGuess = new BufferedReader(kGuess);
-		
-		
-		String userGuess="2";
-		String result = dot.checkYourself(userGuess);
-		
-		String testResult = "failed";
-		
-		if (result.equals("hit")){
-			testResult = "passed";
+
+		// InputStreamReader kGuess = new InputStreamReader(System.in);
+		// BufferedReader userGuess = new BufferedReader(kGuess);
+
+		while (isAlive == true) {
+
+			//String userGuess = "2";
+			String userGuess = helper.getUserInput("enter a number");
+			String result = dot.checkYourself(userGuess);
+			numOfGuesses++;
+
+			if (result.equals("kill")) {
+				isAlive = false;
+				System.out.println("You took " + numOfGuesses + "guesses");
+			}
 		}
-		
-		System.out.println(testResult);
+
 	}
 }
